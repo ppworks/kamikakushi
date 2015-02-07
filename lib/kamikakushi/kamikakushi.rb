fba4fcff7ddb73c07b8a689ecc5a5fc58b13ded9
koshikawa <koshikawa@ppworks.jp>
2015-02-07 22:25:38 +0900

    Allow column_name options for changing deletaion column

--- lib/kamikakushi/kamikakushi.rb
+++ lib/kamikakushi/kamikakushi.rb
@@ -6,7 +6,9 @@ module Kamikakushi
     end
 
     module ClassMethods
-      def kamikakushi
+      def kamikakushi(options = {})
+        options.reverse_merge!(column_name: :deleted_at)
+        define_singleton_method(:kamikakushi_column_name) { options[:column_name] }
         class_eval do
           include InstanceMethods
           default_scope { without_deleted }
@@ -14,15 +16,15 @@ module Kamikakushi
           alias_method_chain :destroyed?, :kamikakushi
 
           scope :with_deleted, -> {
-            unscope(where: :deleted_at)
+            unscope(where: kamikakushi_column_name.to_sym)
           }
 
           scope :without_deleted, -> {
-            where(deleted_at: nil)
+            where(kamikakushi_column_name => nil)
           }
 
           scope :only_deleted, -> {
-            with_deleted.where.not(deleted_at: nil)
+            with_deleted.where.not(kamikakushi_column_name => nil)
           }
         end
       end
@@ -31,7 +33,7 @@ module Kamikakushi
     module InstanceMethods
       def destroy_with_kamikakushi
         run_callbacks(:destroy) do
-          touch(:deleted_at)
+          touch(self.class.kamikakushi_column_name)
         end
       end
 
@@ -40,7 +42,7 @@ module Kamikakushi
       end
 
       def restore
-        update_column(:deleted_at, self.deleted_at = nil)
+        update_column(self.class.kamikakushi_column_name.to_sym, write_attribute(self.class.kamikakushi_column_name, nil))
       end
 
       def purge
